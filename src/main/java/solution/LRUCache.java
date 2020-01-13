e1122c555ea645e65f51b5658e503a6d0093d875
wangfengshu805 <wangfengshu805@berrygenomics.com>
2020-01-13 11:07:48 +0800

    使用双向链表结构+HashMap实现LRU缓存机制

--- src/main/java/solution/LRUCache.java
+++ src/main/java/solution/LRUCache.java
@@ -52,4 +52,64 @@ public class LRUCache {
         removeNode(res);
         return res;
     }
+
+    public LRUCache(int capacity) {
+        this.size = 0;
+        this.capacity = capacity;
+        head = new DLinkedNode();
+        tail = new DLinkedNode();
+
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        DLinkedNode node = cache.get(key);
+        if (node == null) return -1;
+
+        // move the accessed node to the head;
+        moveToHead(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        DLinkedNode node = cache.get(key);
+
+        if (node == null) {
+            DLinkedNode newNode = new DLinkedNode();
+            newNode.key = key;
+            newNode.value = value;
+
+            cache.put(key, newNode);
+            addNode(newNode);
+
+            ++size;
+            if (size > capacity) {
+                // pop the tail
+                DLinkedNode tail = popTail();
+                cache.remove(tail.key);
+                --size;
+            }
+        } else {
+            // update the value.
+            node.value = value;
+            moveToHead(node);
+        }
+    }
 }
+/**
+ * LRUCache 对象会以如下语句构造和调用:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ *
+ * 复杂度分析
+ *
+ * 时间复杂度：对于 put 和 get 都是 O(1)O(1)。
+ * 空间复杂度：O(capacity)O(capacity)，因为哈希表和双向链表最多存储 capacity + 1 个元素。
+ *
+ * 作者：LeetCode
+ * 链接：https://leetcode-cn.com/problems/lru-cache/solution/lru-huan-cun-ji-zhi-by-leetcode/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
